76880f66a715b190c9a50c1b6890535855ca141b
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2024-04-17 16:34:23 -0400

    login/signup links and began styling landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(private router: Router) {}
+
   title = 'cookr-app';
+
+  loginButton() {
+    this.router.navigate(['./login']);
+  }
+
+  signUpButton() {
+    this.router.navigate(['./signup']);
+  }
 }
