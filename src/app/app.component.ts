5c316c9b87b69bcc48d85453b6523fadbd45ad37
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2024-04-18 15:16:09 -0400

    logout functionality, styling for logged in users & link to profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet, Router } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { AuthService } from './auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +15,25 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private BreakpointObserver: BreakpointObserver, private authService: AuthService, private snackBar: MatSnackBar) {
+    this.authService.userAuthenticated$.subscribe(isAuthenticated => {
+      this.userAuthenticated = isAuthenticated;
+    });
+    this.authService.userData$.subscribe(userData => {
+      this.user = userData;
+      console.log("user data", this.user);
+    })
+  }
 
   title = 'cookr-app';
+  userAuthenticated: boolean = false;
+  user: any;
+
+  ngOnInit() {
+    console.log("does this work?", this.authService.userData$);
+
+    this.authService.checkUserAuthStatus();
+  }
 
   loginButton() {
     this.router.navigate(['./login']);
@@ -22,5 +41,26 @@ export class AppComponent {
 
   signUpButton() {
     this.router.navigate(['./signup']);
+  }
+
+  logoutUser() {
+    this.authService.logoutUser((response) => {
+      console.log("response", response);
+      if (response.success) {
+        this.router.navigate(['./']);
+      } else {
+        this.snackBarError('Error logging user out!');
+      }
+    });
+  }
+
+  snackBarError(message: string) {
+    this.snackBar.open(message, 'Close', {
+      duration: 6000,
+    })
+  }
+
+  navUserProfile() {
+    this.router.navigate(['./user/' + this.user.uid])
   }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@angular/core';
 import { getAuth, signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword } from '@angular/fire/auth';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   auth = getAuth();
+  authUser: any = {};
+
+  // set up observables for authenticated status and user data
+  private userAuthenticatedSubject = new BehaviorSubject<boolean>(false);
+  private userSubject = new BehaviorSubject<any>(this.authUser);
+  userAuthenticated$ = this.userAuthenticatedSubject.asObservable();
+  userData$ = this.userSubject.asObservable();
+
+  checkUserAuthStatus() {
+    this.auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.userAuthenticatedSubject.next(true);
+        this.userSubject.next(user);
+      }
+    });
+  }
 
   async loginUser(email: string, password: string, callback: (response: { success: boolean, message: string, id: string}) => void) {
     signInWithEmailAndPassword(this.auth, email, password)
@@ -26,5 +43,17 @@ export class AuthService {
     .catch((error) => {
       callback({ success: false, message: error.code, id: 'NA' });
     });
+  }
+
+  async logoutUser(callback: (response: { success: boolean, message?: string }) => void) {
+    signOut(this.auth)
+    .then(() => {
+      this.userAuthenticatedSubject.next(false);
+      callback({success: true});
+    })
+    .catch((error) => {
+      this.userAuthenticatedSubject.next(true);
+      callback({success: false, message: error});
+    })
   }
 }
